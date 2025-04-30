244b0a89d96a6e3bc75a3da5e0144a1644f1bab0
Joy Zhu <zhunuofei@163.com>
2025-04-30 17:21:06 +1000

    2

--- src/view/login.vue
+++ src/view/login.vue
@@ -1,5 +1,21 @@
 <template>
-    <div>Login</div>
+    <div class="login">
+        <div class ="login_info">
+            <img src="/Users/joy/Desktop/Game/IC private funding/ICprivate-funding/src/assets/login_image.png" alt="logo"/>
+            <h1>Welcome to ETERNITY CAPITAL Portal</h1>
+            <h2>Use your email address to login</h2>
+            <div class="user">
+                <input v-model="User.email" placeholder="Enter your eamil"/>
+             </div>
+            <div class="user">
+                <input v-model="User.password" placeholder="Enter your password"/>
+                <el-icon size="20" color="#808080"><View /></el-iconel-icon>
+            </div>
+            <button @click="login">
+
+     </div>
+    </div>
+
 </template>
 
 <script setup>
