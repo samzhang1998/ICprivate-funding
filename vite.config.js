26ef3050737492d06c2376b66eac6bb04ee9dee8
Your Name <you@example.com>
2025-05-21 11:26:57 +1000

    Fix company assets API integration

--- vite.config.js
+++ vite.config.js
@@ -12,9 +12,9 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://3.25.83.191",
+        target: "http://3.25.83.191:8002", 
         changeOrigin: true,
-        // secure: false,  // 如果是https接口，需要配置这个参数
+        // secure: false,
       },
     },
     cors: true,
